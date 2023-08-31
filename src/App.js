a887cb22a413fc19d823c79d6e2c802a33fb15c8
Roman Tivekeev <rem757@gmail.com>
2023-08-31 20:14:59 +0300

    DevOps

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellow from CICD
+          Hellow from DevOps!
         </p>
         <a
           className="App-link"
